3f2f26fb9582c16ac128a16eb6559b61af13578b
Kirill <kirilltrezubov@yandex.ru>
2020-06-01 22:44:14 +0300

    finish lesson 6 and make homework

--- src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
@@ -1,13 +1,43 @@
 package ru.kirilltrezubov.springcourse;
 
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music> music;
+    private String name;
+    private int volume;
     //IoC
-    public MusicPlayer(Music music) {
+    public MusicPlayer(List<Music> music) {
         this.music = music;
     }
 
+    public MusicPlayer() {}
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+        for (Music music: music)
+            System.out.println("Playing: " + music.getSong());
+    }
+    public void setMusic(List<Music> music){
+        this.music = music;
+    }
+
+    public List<Music> getMusic() {
+        return music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
+

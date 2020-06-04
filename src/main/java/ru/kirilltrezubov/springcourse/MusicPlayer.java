4e97117b8b997d75f1c262bbf4dfaaf811cdb94d
Kirill <kirilltrezubov@yandex.ru>
2020-06-04 04:49:04 +0300

    finish spring lesson 9

--- src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
@@ -1,26 +1,28 @@
 package ru.kirilltrezubov.springcourse;
 
-import java.util.List;
 
 public class MusicPlayer {
-    private List<Music> music;
+    private Music music;
     private String name;
     private int volume;
+
     //IoC
-    public MusicPlayer(List<Music> music) {
+    public MusicPlayer(Music music) {
         this.music = music;
     }
 
-    public MusicPlayer() {}
+    public MusicPlayer() {
+    }
+
     public void playMusic() {
-        for (Music music: music)
-            System.out.println("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong());
     }
-    public void setMusic(List<Music> music){
+
+    public void setMusic(Music music) {
         this.music = music;
     }
 
-    public List<Music> getMusic() {
+    public Music getMusic() {
         return music;
     }
 

694b5326df810c504881c4f5ace8848d8acdd270
Kirill <kirilltrezubov@yandex.ru>
2020-06-05 18:37:36 +0300

    make DI in MusicPlayer with @Qualifier annotation

--- src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Component;
 public class MusicPlayer {
     private Music music1;
     private Music music2;
+    private Music music3;
 
     @Autowired
-    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2, @Qualifier("rapMusic") Music music3) {
         this.music1 = music1;
         this.music2 = music2;
+        this.music3 = music3;
     }
 
     public String playMusic() {
--- src/main/java/ru/kirilltrezubov/springcourse/RapMusic.java
+++ src/main/java/ru/kirilltrezubov/springcourse/RapMusic.java
@@ -1,8 +1,11 @@
 package ru.kirilltrezubov.springcourse;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class RapMusic implements Music{
     @Override
     public List<String> getSong() {

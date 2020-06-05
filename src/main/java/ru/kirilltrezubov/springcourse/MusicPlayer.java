2e6a439dc5bae5339ba0ca8e4e2278d7be923f45
Kirill <kirilltrezubov@yandex.ru>
2020-06-05 18:28:20 +0300

    finishing spring lesson 11

--- src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
+++ src/main/java/ru/kirilltrezubov/springcourse/MusicPlayer.java
@@ -1,51 +1,24 @@
 package ru.kirilltrezubov.springcourse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
-    private ClassicalMusic classicalMusic;
-    private RockMusic rockMusic;
-    private String name;
-    private int volume;
+    private Music music1;
+    private Music music2;
 
-    //IoC
     @Autowired
-    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic) {
-        this.classicalMusic = classicalMusic;
-        this.rockMusic = rockMusic;
-    }
-
-    public MusicPlayer() {
+    public MusicPlayer(@Qualifier("rockMusic") Music music1, @Qualifier("classicalMusic") Music music2) {
+        this.music1 = music1;
+        this.music2 = music2;
     }
 
     public String playMusic() {
-        return "Playing: " + classicalMusic.getSong();
+        return "Playing: " + music1.getSong() + ", " + music2.getSong();
     }
 
-    public void setMusic(ClassicalMusic music) {
-        this.classicalMusic = music;
-    }
 
-    public Music getMusic() {
-        return classicalMusic;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
 }
 
